98380407e2bc97863c6c1599651bfe4f1c232b73
Cassio Seffrin <cassioseffrin@gmail.com>
2020-06-26 19:37:47 -0300

    ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Contato from './componentes/Contato';
 import './App.css';
-class App extends Component {
-  constructor(props) {
-    super(props);    
-    this.state = {
-      clientes: []
-    };
-  }
-  componentDidMount(){
-    var myHeaders = new Headers();
-    myHeaders.append("X-Parse-Application-Id", "aulaparse");
-    myHeaders.append("X-Parse-Master-Key", "aulaparse");
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-    fetch("https://aws.magnani.ind.br:1337/parse/classes/Cliente", requestOptions)
-      .then(response => response.json())
-      .then(res => this.setState({clientes: res.results} ))
-      .catch(error => console.log('error', error));
-  }
-  render() {
-    return (
-      <div className="App">
-        <div>
-        <Contato  clientes={this.state.clientes}  />
-        </div>
-      </div>
-    );
-  }
+class App extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			clientes: []
+		};
+	}
+
+	componentWillMount() {}
+
+	componentDidMount() {
+		var myHeaders = new Headers();
+		myHeaders.append('X-Parse-Application-Id', 'aulaparse');
+		myHeaders.append('X-Parse-Master-Key', 'aulaparse');
+		var requestOptions = {
+			method: 'GET',
+			headers: myHeaders,
+			redirect: 'follow'
+		};
+		fetch('https://aws.magnani.ind.br:1337/parse/classes/Cliente', requestOptions)
+			.then((response) => response.json())
+			.then((res) => this.setState({ clientes: res.results }))
+			.catch((error) => console.log('error', error));
+	}
+	render() {
+		return (
+			<div className="App">
+				<div>
+					<Contato clientes={this.state.clientes} />
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
